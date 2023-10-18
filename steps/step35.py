170daa54a7aa6a968bb6a5a51aa9cd256f09cf74
GoGiants1 <chooi221@snu.ac.kr>
2023-10-18 19:09:46 +0900

    Done Step 24~36

--- steps/step35.py
+++ steps/step35.py
@@ -1,6 +1,7 @@
-if '__file__' in globals():
+if "__file__" in globals():
     import os, sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import numpy as np
 from dezero import Variable
 from dezero.utils import plot_dot_graph
@@ -8,11 +9,11 @@ import dezero.functions as F
 
 x = Variable(np.array(1.0))
 y = F.tanh(x)
-x.name = 'x'
-y.name = 'y'
+x.name = "x"
+y.name = "y"
 y.backward(create_graph=True)
 
-iters = 1
+iters = 0
 
 for i in range(iters):
     gx = x.grad
@@ -20,5 +21,5 @@ for i in range(iters):
     gx.backward(create_graph=True)
 
 gx = x.grad
-gx.name = 'gx' + str(iters + 1)
-plot_dot_graph(gx, verbose=False, to_file='tanh.png')
+gx.name = "gx" + str(iters + 1)
+plot_dot_graph(gx, verbose=False, to_file=f"tanh_{iters + 1}.png")
